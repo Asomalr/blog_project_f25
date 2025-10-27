512719471227893c697a5b62c5513ee84aea2c91
Mohamamd AlMarzouq <mo.almarzouq@ku.edu.kw>
2025-10-27 08:55:07 +0300

    Update models.py

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS =(
+    (0, "Draft"),
+    (1, "Published"),
+)
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    body = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
